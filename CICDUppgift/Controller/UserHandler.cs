7b3e4793342b42a204296503f0713206bda58ad6
Douglas <94dousch@gafe.molndal.se>
2021-10-12 14:51:25 +0200

    Fullständigt klart.

--- CICDUppgift/Controller/UserHandler.cs
+++ CICDUppgift/Controller/UserHandler.cs
@@ -5,33 +5,51 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
-    using System.Threading.Tasks;
     using System.IO;
-    using System.Threading;
-    using CICDUppgift.View;
 
     public class UserHandler
     {
+        /// <summary>
+        /// User text fil path.
+        /// </summary>
         public static string userPath = "..//..//..//..//CICDUppgift//Users.txt";
-        public static string AdminLog = "..//..//..//..//CICDUppgift//AdminLog.txt";
+        /// <summary>
+        /// AdminLog text fil path.
+        /// Till för admin att se förändringar som användare vill utföra.
+        /// </summary>
+        private static string AdminLog = "..//..//..//..//CICDUppgift//AdminLog.txt";
 
+        /// <summary>
+        /// Loggar in användare, kollar i listan så användare finns och stämmer
+        /// Likadant för lösenord.
+        /// Stämmer allt, loggas man in, annars ej.
+        /// </summary>
+        /// <param name="username">Användarnamn</param>
+        /// <param name="password">Lösenord</param>
+        /// <returns></returns>
         public static User LoginUser(string username, string password)
         {
             List<User> Users = GetAllUsersToList();
 
             foreach (var user in Users)
             {
-
                 if (user.userName == username && user.password == password)
                 {
-
                     return user;
                 }
             }
             return null;
         }
 
-
+        /// <summary>
+        /// Sparar en förändring som användare vill utföra.
+        /// Ex: Ändra Roll eller sin lön.
+        /// </summary>
+        /// <param name="userName">Användarnamn</param>
+        /// <param name="userRole">Roll</param>
+        /// <param name="typeOfChange">Typ av förändring</param>
+        /// <param name="currentValue">Nuvarande värde (Roll eller lön)</param>
+        /// <param name="newValue">Det förändrade värdet.</param>
         public static void SaveChangeRequest(string userName, string userRole, string typeOfChange, string currentValue, string newValue)
         {
             string request = userName + ":" + userRole + ":" + typeOfChange + ":" + currentValue + ":" + newValue;
@@ -42,7 +60,10 @@
             }
         }
 
-
+        /// <summary>
+        /// Hämtar alla användare, som sedan sparar till en lista som user objekt.
+        /// </summary>
+        /// <returns>En användare</returns>
         public static List<User> GetAllUsersToList()
         {
             List<string> Users = new List<string>();
@@ -80,20 +101,11 @@
             return userUsers;
         }
 
-        //public static User GetUser(int ID)
-        //{
-        //    List<User> users = GetAllUsersToList();
-
-        //    foreach (var user in users)
-        //    {
-        //        if (user.ID == ID)
-        //        {
-        //            return user;
-        //        }
-        //    }
-        //    return null;
-        //}
-
+        /// <summary>
+        /// Kollar om en användare finns i systemet.
+        /// </summary>
+        /// <param name="username">Användarnamn</param>
+        /// <returns>True om hen finns, annars false</returns>
         public static bool CheckIfUsernameExists(string username)
         {
             List<User> users = GetAllUsersToList();
@@ -108,53 +120,43 @@
             return false;
         }
 
+        /// <summary>
+        /// Radderar en användare om man anger sitt:
+        /// Användarnamn och lösenord.
+        /// Sedan skrivs allt över i systemet och uppdateras.
+        /// </summary>
+        /// <param name="username">Användarnamn</param>
+        /// <param name="password">Lösenord</param>
         public static void DeleteUser(string username, string password)
         {
-            // Hämtar users
             List<User> users = GetAllUsersToList();
 
             users.RemoveAll(u => u.userName == username && u.password == password);
             OverwriteSavedUsers(users);
-
-
-
-
         }
 
+        /// <summary>
+        /// Lägger till en ny användare.
+        /// </summary>
+        /// <param name="userName">Användarnamn</param>
+        /// <param name="password">Lösenord</param>
+        /// <param name="role">Roll</param>
+        /// <param name="salary">Lön</param>
+        /// <param name="accountType">Konto typ (User / Admin).</param>
         public static void AddNewUser(string userName, string password, string role, int salary, string accountType)
         {
-
             string user = GetNewUserID() + ":" + userName + ":" + password + ":" + role + ":" + salary + ":" + 0 + ":" + accountType;
             
             using (StreamWriter sw = new StreamWriter(userPath, true))
             {
                 sw.WriteLine(user);
-                //sw.Close();
             }
         }
 
         /// <summary>
-        /// Skriver användare till txt
+        /// Skriver över sparade användare med ny uppdaterad data.
         /// </summary>
-        /// <param name="users"></param>
-        //public static void listOfUser(List<User> users)
-        //{
-
-
-        //    foreach (var item in users)
-        //    {
-        //        using (StreamWriter sw = new StreamWriter(userPath, true))
-        //        {
-        //            string user = newID() + ":" + item.userName + ":" + item.password + ":" + item.role + ":" + item.salary + ":" + item.balance + ":" + item.accountType;
-
-        //            sw.WriteLine(user);
-        //            sw.Close();
-        //        };
-
-        //    }
-
-        //}
-
+        /// <param name="users">Användare</param>
         public static void OverwriteSavedUsers(List<User> users)
         {
             NewUserTxt();
@@ -166,31 +168,28 @@
                     string user = GetNewUserID() + ":" + item.userName + ":" + item.password + ":" + item.role + ":" + item.salary + ":" + item.balance + ":" + item.accountType;
 
                     sw.WriteLine(user);
-                    sw.Close();
-                };
-
+                }
             }
-
         }
 
+        /// <summary>
+        /// Skapar en ny tom sparning av användare, med enbart admin.
+        /// </summary>
         public static void NewUserTxt()
         {
-
             using (StreamWriter sw = new StreamWriter(userPath, false))
             {
                 string user = 1 + ":" + "admin1" + ":" + "admin1234" + ":" + "Admin" + ":" + 0 + ":" + 0 + ":" + "Admin";
                 sw.WriteLine(user);
-                sw.Close();
             }
-
-
-
         }
 
+        /// <summary>
+        /// När en ny användare skapas, får hen ett uppdaterat ID i systemet.
+        /// </summary>
+        /// <returns>ID + 1</returns>
         public static int GetNewUserID()
         {
-            // List<string> Users = Users = File.ReadAllLines(userPath).ToList();
-
             List<string> Users = new List<string>();
             using (var stream = new FileStream(userPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             {
@@ -203,20 +202,18 @@
                     }
                 }
             }
-
-
-
-
             var lastID = Users.Last().Split(':');
             int ID = Convert.ToInt32(lastID[0]);
 
             return ID + 1;
-
         }
 
+        /// <summary>
+        /// Kontrollerar att man enbart får skriva siffror
+        /// </summary>
+        /// <returns>Input</returns>
         public static int GetDigitInput()
         {
-
             int userInput;
             bool parsed = false;
             do
@@ -227,14 +224,16 @@
                 {
                     Console.WriteLine("Incorrect input");
                 }
-
             }
             while (!parsed);
 
             return userInput;
-
         }
 
+        /// <summary>
+        /// Kontrollerar att man enbart får skriva bokstäver
+        /// </summary>
+        /// <returns>Input</returns>
         public static string GetLetterInput()
         {
             string userInput;
@@ -247,13 +246,16 @@
                 {
                     Console.WriteLine("Incorrect Input!");
                 }
-
             }
             while (!result);
 
             return userInput;
         }
 
+        /// <summary>
+        /// Kontrollerar att man enbart får skriva både siffror och bokstäver.
+        /// </summary>
+        /// <returns>Input</returns>
         public static string GetNumberAndLetterInput()
         {
             string userInput;
@@ -276,6 +278,11 @@
             return userInput;
         }
 
+        /// <summary>
+        /// Hämtar data från AdminLog filen för att se det användaren vill förändra
+        /// på sitt konto.
+        /// </summary>
+        /// <returns>Användarens förfrågan</returns>
         public static List<UserRequest> GetAdminLog()
         {
             List<string> requests = new List<string>();
@@ -309,13 +316,15 @@
                 userRequests.Add(currentRequest);
             }
             return userRequests;
-
         }
 
+        /// <summary>
+        /// Skriver över gammal data till ny data som användaren vill förändra.
+        /// </summary>
+        /// <param name="request">Förfrågan</param>
         public static void ExecuteChangeRequest(UserRequest request)
         {
             var users = GetAllUsersToList();
-
 
             foreach (var user in users.Where(u => u.userName == request.userName))
             {
@@ -330,9 +339,12 @@
             }
 
             OverwriteSavedUsers(users);
-
         }
 
+        /// <summary>
+        /// Skriver över AdminLog med ny data
+        /// </summary>
+        /// <param name="requests">Förfrågan</param>
         public static void OverwriteSavedChangeRequests(List<UserRequest> requests)
         {
             NewAdminlogTxt();
@@ -344,24 +356,25 @@
                     string request = item.userName + ":" + item.role + ":" + item.change + ":" + item.currentValue + ":" + item.newValue;
                     
                     sw.WriteLine(request);
-                    sw.Close();
-                   
-                };
-
+                }
             }
-
         }
 
+        /// <summary>
+        /// Skapar en ny tom AdminLog fil
+        /// </summary>
         public static void NewAdminlogTxt()
         {
-
             using (StreamWriter sw = new StreamWriter(AdminLog, false))
             {
-                
                 sw.WriteLine("username:role:change:currentValue:newValue");
-                sw.Close();
             }
         }
+
+        /// <summary>
+        /// Betalar ut lön till alla i systemet.
+        /// </summary>
+        /// <returns>Den totala lönen</returns>
         public static int PayOutAllSalaries()
         {
             var users = GetAllUsersToList();
--- CICDUppgift/Model/Account.cs
+++ CICDUppgift/Model/Account.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CICDUppgift.Model
+﻿namespace CICDUppgift.Model
 {
+    /// <summary>
+    /// Konto modell för Användare/Admin.
+    /// </summary>
     public class Account
     {
-        public int ID { get; set; }
         public string userName { get; set; }
         public string password { get; set; }
-        public string role { get; set; }
         public string accountType { get; set; }
-        
     }
 }
